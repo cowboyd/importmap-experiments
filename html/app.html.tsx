d16caade06539758d1fb1f36380836287e46b86f
Charles Lowell <cowboyd@frontside.com>
2023-08-29 15:51:54 -0500

    Access the search params

--- html/app.html.tsx
+++ html/app.html.tsx
@@ -1,8 +1,11 @@
 import type { Operation } from "effection";
+import { useURL } from "freejack/view.ts";
 
 export function* AppHtml(): Operation<JSX.Element> {
   let title = "Micro Frontend Composer";
   let description = "Prototyping using Import Maps to create A Micro Frontend Architecture";
+
+  let url = yield* useURL();
   return (
     <html lang="en-US" dir="ltr">
       <head>
@@ -16,8 +19,8 @@ export function* AppHtml(): Operation<JSX.Element> {
         <link rel="stylesheet" href="https://use.typekit.net/ugs0ewy.css" />
       </head>
       <body>
-        <main class="text-center">
-          <h1>Hello World</h1>
+        <main class="prose m-auto">
+          <h1>params: {JSON.stringify([...url.searchParams.entries()])}</h1>
         </main>
       </body>
     </html>
